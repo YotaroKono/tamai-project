82023fb4bb8c5dd8394c98888e79efed8ce2628b
YotaroKono <yotarokono@gmail.com>
2026-01-26 00:07:31 +0900

    Add theming support with react-native-paper and update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,10 @@
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-
+import { PaperProvider } from "react-native-paper";
 import { useSupabase } from "@/hooks/useSupabase";
 import { SupabaseProvider } from "@/providers/supabase-provider";
-
-SplashScreen.setOptions({
-	duration: 500,
-	fade: true,
-});
+import { paperTheme } from "@/theme/paperTheme";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,21 +26,23 @@ function RootNavigator() {
 	}, [isLoaded]);
 
 	return (
-		<Stack
-			screenOptions={{
-				headerShown: false,
-				gestureEnabled: false,
-				animation: "none",
-				animationDuration: 0,
-			}}
-		>
-			<Stack.Protected guard={!!session}>
-				<Stack.Screen name="(protected)" />
-			</Stack.Protected>
+		<PaperProvider theme={paperTheme}>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+					gestureEnabled: false,
+					animation: "none",
+					animationDuration: 0,
+				}}
+			>
+				<Stack.Protected guard={isAuthenticated}>
+					<Stack.Screen name="(protected)" />
+				</Stack.Protected>
 
-			<Stack.Protected guard={!session}>
-				<Stack.Screen name="(public)" />
-			</Stack.Protected>
-		</Stack>
+				<Stack.Protected guard={!isAuthenticated}>
+					<Stack.Screen name="(public)" />
+				</Stack.Protected>
+			</Stack>
+		</PaperProvider>
 	);
 }
