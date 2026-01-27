6a59fc140188e98f2686eb786ae68b5bce9e7681
Sora Harada <92529068+Saku052@users.noreply.github.com>
2026-01-27 16:15:50 +0900

    Feature/user data accept (#22)
    * random commmit1
    
    * Improve/itemfunction（部分的なUIの修正） (#20)
    
    * fix: update secondaryContainer color and add bottom sheet styles
    
    * refactor: improve styling and layout in ShoppingScreen component
    
    * refactor: streamline ItemBottomSheet component and improve form handling
    
    * refactor: enhance ItemBottomSheet component with overlay and improved structure
    
    * wexyo
    
    * Refactor ItemBottomSheet to use Modal and improve keyboard handling
    
    * add some stuff
    
    ---------
    
    Co-authored-by: yotaro <117880561+YotaroKono@users.noreply.github.com>
    Co-authored-by: reo001122 <reo001122@gmail.com>
    Co-authored-by: YotaroKono <yotarokono@gmail.com>

--- features/group/index.ts
+++ features/group/index.ts
@@ -2,4 +2,5 @@ export * from "./group.api";
 export * from "./hooks/useCreateGroup";
 export * from "./hooks/useJoinGroup";
 export * from "./hooks/useUserGroups";
+export * from "./hooks/useUserSpace";
 export * from "./types";
